a7f75546f4586b17c7c26ee5f95235096920988d
Martin Mwangi <mwangimartin1904@gmail.com>
2022-01-04 14:55:24 +0300

    Updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, NavLink, useLocation } from "react-router-dom";
 import { FaSearch, FaCalendarAlt, FaBars, FaChurch } from "react-icons/fa";
+import { HiChevronRight, HiChevronDown } from "react-icons/hi";
 
 const NavLinkItem = ({ to, title, closeMenu, ...rest }) => {
   const { pathname } = useLocation();
@@ -18,6 +19,40 @@ const NavLinkItem = ({ to, title, closeMenu, ...rest }) => {
     >
       {title}
     </NavLink>
+  );
+};
+
+const NavDropDownLinkItem = ({ to, title, children, ...rest }) => {
+  const [active, setActive] = useState(false);
+  const { pathname } = useLocation();
+  return (
+    <div
+      onClick={() => setActive(!active)}
+      className={`w-full md:w-auto cursor-pointer md:relative group text-sm md:text-md my-2  md:px-4 uppercase font-medium md:h-14 flex flex-col md:flex-row md:items-center ${
+        pathname === to
+          ? "text-slate-100 bg-dodge-blue "
+          : "text-slate-100 md:text-slate-600 "
+      }" hover:bg-dodge-blue hover:text-slate-100
+      ${pathname === "/" ? "text-slate-100 md:text-slate-100" : ""}`}
+      title={title}
+      {...rest}
+    >
+      <div className="flex justify-between items-center">
+        <span>{title}</span>
+        <span className="md:hidden ml-20">
+          {active ? (
+            <HiChevronDown className="text-xl" />
+          ) : (
+            <HiChevronRight className="text-xl" />
+          )}
+        </span>
+      </div>
+      <div className={`${active ? "block my-4 ml-4" : "hidden md:block"}`}>
+        <div className="md:absolute md:px-4 flex-col md:hidden flex md:group-hover:flex md:top-full md:left-0 w-auto h-auto bg-dodge-blue">
+          {children}
+        </div>
+      </div>
+    </div>
   );
 };
 
@@ -44,7 +79,7 @@ const Header = () => {
               alt="ggggg"
             /> */}
             <FaChurch className="text-4xl mr-2 text-slate-200" />
-            <div className="flex flex-col gap-0 my-2">
+            <div className="hidden sm:flex flex-col gap-0 my-2">
               <span className="text-slate-200 tracking-wider md:font-bold text-xs">
                 Blessed Sister Irene Stefani
               </span>
@@ -52,6 +87,7 @@ const Header = () => {
                 Catholic Chaplaincy
               </span>
             </div>
+            <h4 className="text-white sm:hidden">DEKUT CC</h4>
           </Link>
           <div className="flex items-center space-x-4">
             <Link
@@ -60,7 +96,10 @@ const Header = () => {
             >
               Daily Readings
             </Link>
-            <Link className="text-md uppercase text-slate-100" to="/events">
+            <Link
+              className="hidden sm:inline-block text-md uppercase text-slate-100"
+              to="/events"
+            >
               <FaCalendarAlt className="h-8 w-3 mx-4" />
             </Link>
             <Link
@@ -71,16 +110,16 @@ const Header = () => {
             </Link>
             <div className="flex">
               <Link
-                className="text-[12px] sm:text-sm md:text-md uppercase text-slate-100 bg-[#236092] h-14 flex items-center px-6"
+                className="text-[12px] sm:text-sm md:text-md uppercase text-slate-100 bg-[#236092] h-14 flex items-center px-4 sm:px-6"
                 to="/library"
               >
                 Library
               </Link>
               <Link
-                className="text-[12px] sm:text-sm md:text-md uppercase text-slate-100 bg-[#407b54] h-14 flex items-center px-6"
+                className="text-[12px] sm:text-sm md:text-md uppercase text-slate-100 bg-[#407b54] h-14 flex items-center px-4 sm:px-6"
                 to="/login"
               >
-                Login | Register
+                Login
               </Link>
             </div>
             <FaSearch className="hidden md:block ml-2 h-8 w-5 text-slate-100" />
@@ -134,23 +173,52 @@ const Header = () => {
           >
             Daily Readings
           </Link>
-          <Link
-            onClick={() => setIsMenuOpen(false)}
-            className=" md:hidden my-2 text-sm uppercase text-slate-100"
-            to="/mass"
-          >
-            Find Mass
-          </Link>
-          <NavLinkItem
-            closeMenu={() => setIsMenuOpen(false)}
-            to="/"
-            title="about dekutcc"
-          />
-          <NavLinkItem
-            closeMenu={() => setIsMenuOpen(false)}
-            to="/d"
-            title="Prayers & Worship"
-          />
+          <NavDropDownLinkItem title="about dekutcc" to="/">
+            <Link
+              onClick={() => setIsMenuOpen(false)}
+              className={`my-2 text-sm uppercase text-slate-100`}
+              to="/leadership"
+            >
+              Leadership
+            </Link>
+            <Link
+              onClick={() => setIsMenuOpen(false)}
+              className=" my-2 text-sm uppercase text-slate-100"
+              to="/events"
+            >
+              Events
+            </Link>
+            <Link
+              onClick={() => setIsMenuOpen(false)}
+              className=" my-2 text-sm uppercase text-slate-100"
+              to="/contact"
+            >
+              Contact
+            </Link>
+          </NavDropDownLinkItem>
+          <NavDropDownLinkItem title="Prayers & Worship">
+            <Link
+              onClick={() => setIsMenuOpen(false)}
+              className={`my-2 text-sm uppercase text-slate-100`}
+              to="/calendar"
+            >
+              Daily readings calendar
+            </Link>
+            <Link
+              onClick={() => setIsMenuOpen(false)}
+              className=" my-2 text-sm uppercase text-slate-100"
+              to="/events"
+            >
+              Prayer
+            </Link>
+            <Link
+              onClick={() => setIsMenuOpen(false)}
+              className=" my-2 text-sm uppercase text-slate-100"
+              to="/contact"
+            >
+              Our Mass
+            </Link>
+          </NavDropDownLinkItem>
           <NavLinkItem
             closeMenu={() => setIsMenuOpen(false)}
             to="/f"
@@ -171,13 +239,6 @@ const Header = () => {
             to="/gallery"
             title="Gallery"
           />
-          <Link
-            onClick={() => setIsMenuOpen(false)}
-            className={`md:hidden my-2 text-sm uppercase text-slate-100`}
-            to="/contact-us"
-          >
-            Contact Us
-          </Link>
           <NavLinkItem
             closeMenu={() => setIsMenuOpen(false)}
             to="/resources"
