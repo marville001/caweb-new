ffcbdbc6fa983bb5ea3ca4ad15531d64b0115a5c
Martin Mwangi <mwangimartin1904@gmail.com>
2022-01-04 10:25:59 +0300

    Updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -37,6 +38,155 @@ const Footer = () => {
             >
               Learn More
             </Link>
+          </div>
+        </div>
+
+        {/* Links */}
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-6">
+          <div>
+            <div className="text-2xl text-blue-300">Prayer & Worship</div>
+            <div className="flex flex-col">
+              <Link
+                className="text-base text-slate-100 font-light"
+                to="/readings/calendar"
+              >
+                Daily Readings Calendar
+              </Link>
+
+              <Link
+                className="text-base text-slate-100 font-light"
+                to="/readings/calendar"
+              >
+                Prayers
+              </Link>
+              <Link
+                className="text-base text-slate-100 font-light"
+                to="/readings/calendar"
+              >
+                Our Mass
+              </Link>
+              <Link
+                className="text-base text-slate-100 font-light"
+                to="/readings/calendar"
+              >
+                The Bible
+              </Link>
+              <Link
+                className="text-base text-slate-100 font-light"
+                to="/readings/calendar"
+              >
+                Our Books Store
+              </Link>
+            </div>
+          </div>
+
+          <div>
+            <div className="text-2xl text-blue-300">Get Involved to Act Now</div>
+            <div className="flex flex-col">
+              <Link
+                className="text-base text-slate-100 font-light"
+                to="/readings/calendar"
+              >
+                Take Action
+              </Link>
+
+              <Link
+                className="text-base text-slate-100 font-light"
+                to="/readings/calendar"
+              >
+                Help Now
+              </Link>
+              <Link
+                className="text-base text-slate-100 font-light"
+                to="/readings/calendar"
+              >
+                Meetings & Events
+              </Link>
+              <Link
+                className="text-base text-slate-100 font-light"
+                to="/readings/calendar"
+              >
+                Pray
+              </Link>
+            </div>
+          </div>
+
+          <div>
+            <div className="text-2xl text-blue-300">Quick Links</div>
+            <div className="flex flex-col">
+              <Link
+                className="text-base text-slate-100 font-light"
+                to="/readings/calendar"
+              >
+                Calendars
+              </Link>
+
+              <Link
+                className="text-base text-slate-100 font-light"
+                to="/readings/calendar"
+              >
+                Social Media
+              </Link>
+              <Link
+                className="text-base text-slate-100 font-light"
+                to="/readings/calendar"
+              >
+                DEKUTCC News Service
+              </Link>
+              <Link
+                className="text-base text-slate-100 font-light"
+                to="/readings/calendar"
+              >
+                About Us
+              </Link>
+              <Link
+                className="text-base text-slate-100 font-light"
+                to="/readings/calendar"
+              >
+                Contact Us
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        {/* Copyrigght and social media */}
+        <div className="flex items-center my-5 justify-between flex-col sm:flex-row gap-5">
+          <p className="text-slate-300 capitalize">
+            @{new Date().getFullYear()} DEKUT CATHOLIC ACTION
+          </p>
+          <div className="flex items-center gap-2">
+            <a
+              href="#youtube"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-slate-300 p-2 rounded-full cursor-pointer"
+            >
+              <FaFacebookF className="text-lg text-white rounded-full" />
+            </a>
+            <a
+              href="#youtube"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-slate-300 p-2 rounded-full cursor-pointer"
+            >
+              <FaYoutube className="text-lg text-white rounded-full" />
+            </a>
+            <a
+              href="#youtube"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-slate-300 p-2 rounded-full cursor-pointer"
+            >
+              <FaTwitter className="text-lg text-white rounded-full" />
+            </a>
+            <a
+              href="#youtube"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-slate-300 p-2 rounded-full cursor-pointer"
+            >
+              <FaInstagram className="text-lg text-white rounded-full" />
+            </a>
           </div>
         </div>
       </div>
