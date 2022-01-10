d95b1e169d003b9e32a11b4008a32a184a0981e4
Martin Mwangi <mwangimartin1904@gmail.com>
2022-01-10 14:36:19 +0300

    Created about route

---
+++ src/pages/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container my-12">
+      <h1 className="w-10/12 mx-auto text-center text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold tracking-wide text-dodge-blue leading-8 xl:leading-tight">
+        We Promote the Common Good Which the Church Offers Humankind
+      </h1>
+      <p className="mt-6 w-10/12 mx-auto md:w-9/12 lg:w-8/12  text-xl text-center leading-8 tracking-wide">
+        The Dekut Catholic Chaplaincy is an Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Reiciendis velit amet accusantium debitis
+        numquam. Esse et atque doloribus repellendus, dolore laboriosam maiores
+        porro architecto ullam aspernatur neque similique, magnam excepturi.
+      </p>
+    </div>
+  );
+};
+
+export default About;
