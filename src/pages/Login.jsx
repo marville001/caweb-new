a7d5381285c0842559db48189b174c9bbaa2c7fe
Martin Mwangi <mwangimartin1904@gmail.com>
2022-01-17 21:16:04 +0300

    Forgot password pafge

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
           <button className="w-full p-2 mt-5 text-white text-center text-lg uppercase bg-dodge-blue rounded cursor-pointer">Login</button>
           <div className="flex justify-between mt-4">
               <Link className="text-md font-medium text-slate-700 underline" to="/register">Register here</Link>
-              <Link className="text-md font-medium text-slate-700 underline" to="/forgot-pass">Forgot password?</Link>
+              <Link className="text-md font-medium text-slate-700 underline" to="/forgot-password">Forgot password?</Link>
           </div>
         </div>
       </div>
