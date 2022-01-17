f3c7f4e0dd00c7d9ae125ea9c202b0742607674d
Martin Mwangi <mwangimartin1904@gmail.com>
2022-01-17 21:11:33 +0300

    complete register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,38 +3,69 @@ import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div className="flex justify-center items-center min-h-[70vh]">
-      <div className="shadow-md p-5 bg-white min-w-[400px]">
+    <div className="flex justify-center items-center min-h-[70vh] my-6">
+      <div className="shadow-md p-5 bg-white min-w-[400px] md:min-w-[600px]">
         <h1 className="text-center font-bold text-3xl text-dodge-blue">
-          Welcome, Login
+          Create An Account
         </h1>
         <div className="py-6">
-          <div className="form-group my-4">
-            <label htmlFor="username" className="text-lg text-slate-600">
-              Username
-            </label>
-            <input
-              type="text"
-              className="p-2 block border-slate-200 border-2 w-full rounded mt-2"
-              placeholder="Enter your username..."
-            />
+          <div className="md:flex md:gap-2">
+            <div className="form-group my-4 flex-1">
+              <label htmlFor="firstname" className="text-lg text-slate-600">
+                Firstname
+              </label>
+              <input
+                type="text"
+                className="p-2 block border-slate-200 border-2 w-full rounded mt-2"
+                placeholder="Enter your firstname..."
+              />
+            </div>
+            <div className="form-group my-4 flex-1">
+              <label htmlFor="lastname" className="text-lg text-slate-600">
+                Lastname
+              </label>
+              <input
+                type="text"
+                className="p-2 block border-slate-200 border-2 w-full rounded mt-2"
+                placeholder="Enter your lastname..."
+              />
+            </div>
           </div>
-          <div className="form-group my-4">
-            <label htmlFor="password" className="text-lg text-slate-600">
-              Password
-            </label>
-            <input
-            id="password"
-              type="password"
-              className="p-2 block border-slate-200 border-2 w-full rounded mt-2"
-              placeholder="Enter your password..."
-            />
+          <div className="md:flex md:gap-2">
+            <div className="form-group my-4 flex-1">
+              <label htmlFor="email" className="text-lg text-slate-600">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="p-2 block border-slate-200 border-2 w-full rounded mt-2"
+                placeholder="Enter your email..."
+              />
+            </div>
+            <div className="form-group my-4 flex-1">
+              <label htmlFor="password" className="text-lg text-slate-600">
+                Password
+              </label>
+              <input
+                id="password"
+                type="password"
+                className="p-2 block border-slate-200 border-2 w-full rounded mt-2"
+                placeholder="Enter your password..."
+              />
+            </div>
           </div>
 
-          <button className="w-full p-2 mt-5 text-white text-center text-lg uppercase bg-dodge-blue rounded cursor-pointer">Login</button>
+          <button className="w-full p-2 mt-5 text-white text-center text-lg uppercase bg-dodge-blue rounded cursor-pointer">
+            Register
+          </button>
           <div className="flex justify-between mt-4">
-              <Link className="text-md font-medium text-slate-700 underline" to="/register">Register here</Link>
-              <Link className="text-md font-medium text-slate-700 underline" to="/forgot-pass">Forgot password?</Link>
+            <Link
+              className="text-md font-medium text-slate-700 underline"
+              to="/login"
+            >
+              Login here
+            </Link>
           </div>
         </div>
       </div>
