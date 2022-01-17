513875934e433f9bc24a33564fe8ca4683bd9642
Martin Mwangi <mwangimartin1904@gmail.com>
2022-01-17 23:01:28 +0300

    Update styling

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -34,7 +34,7 @@ const Events = () => {
         {/* Events listing */}
         <div className="my-10 divide-y-2 divide-gray-200ra">
           {[1, 2, 3, 4, 5, 6].map((i) => (
-            <div className="flex flex-col md:flex-row md:divide-x-2 divide-gray-300 py-4 hover:bg-white cursor-pointer hover:shadow-lg px-4">
+            <div className="flex flex-col md:flex-row md:divide-x-2 divide-gray-300 py-4 hover:bg-white cursor-pointer shadow-all px-4">
               <div className="md:pr-6 min-w-[150px]">
                 <h3 className="text-slate-800 text-center font-bold text-2xl md:text-3xl">
                   {20 + i}-{22 + i}
