89e2ec8885c3083695caa2d6c9534edcb0f9203b
Martin Mwangi <mwangimartin1904@gmail.com>
2022-01-19 08:45:18 +0300

    Updates

--- src/pages/DailyReadings.jsx
+++ src/pages/DailyReadings.jsx
@@ -9,9 +9,9 @@ const DailyReadings = () => {
       <h1 className="text-3xl md:text-4xl text-center text-dodge-blue font-bold">
         Daily Readings
       </h1>
-
-      {/* Header */}
-      <div className="flex justify-between">
+      <div className="bg-slate-50 my-5 p-8">
+        {/* Header */}
+        <div className="flex justify-between">
           <div className="flex flex-col md:flex-row gap-2 md:ml-12">
             <Link
               className="border-slate-800 border-2 p-2 px-4 uppercase text-slate-800 hover:bg-slate-800 hover:text-white font-medium text-md"
@@ -22,20 +22,21 @@ const DailyReadings = () => {
           </div>
         </div>
 
-      {/* Listing */}
-      <CalendarListing daily />
+        {/* Listing */}
+        <CalendarListing daily />
 
-      {/* Page switch */}
-      <div className="flex justify-between items-center px-6 w-10/12 md:w-5/12 mx-auto">
-        <span className="flex items-center cursor-pointer">
-          <FaChevronCircleLeft className="mt-1 mr-2 text-gray-400 text-xl" />
-          <span className="hidden sm:block">previous</span>
-        </span>
-        <h2 className="text-xl font-medium">Page 1</h2>
-        <span className="flex items-center cursor-pointer">
-          <span className="hidden sm:block">next</span>
-          <FaChevronCircleRight className="mt-1 ml-2 text-gray-800 text-xl" />
-        </span>
+        {/* Page switch */}
+        <div className="flex justify-between items-center px-6 w-10/12 md:w-5/12 mx-auto">
+          <span className="flex items-center cursor-pointer">
+            <FaChevronCircleLeft className="mt-1 mr-2 text-gray-400 text-xl" />
+            <span className="hidden sm:block">previous</span>
+          </span>
+          <h2 className="text-xl font-medium">Page 1</h2>
+          <span className="flex items-center cursor-pointer">
+            <span className="hidden sm:block">next</span>
+            <FaChevronCircleRight className="mt-1 ml-2 text-gray-800 text-xl" />
+          </span>
+        </div>
       </div>
     </div>
   );
