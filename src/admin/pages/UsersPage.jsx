10935512fc6ff4716700a91f76a5ec8a06542607
Martin Mwangi <mwangimartin1904@gmail.com>
2022-01-24 00:01:50 +0300

    Add search input

--- src/admin/pages/UsersPage.jsx
+++ src/admin/pages/UsersPage.jsx
@@ -17,14 +17,23 @@ const UsersPage = () => {
   return (
     <div className="relative">
       <div className="align-middle inline-block min-w-full mt-6">
-        <div className="p-4 flex justify-between">
-          <h4 className=" text-2xl font-bold">Users List</h4>
-          <button
-            onClick={openModal}
-            className="p-2 bg-sea-green py-1 px-4 text-white uppercase font-normal rounded-md"
-          >
-            Add User
-          </button>
+        <div className="bg-white rounded-t-md">
+          <div className="p-4 flex justify-between ">
+            <h4 className=" text-2xl font-bold">Users List</h4>
+            <button
+              onClick={openModal}
+              className="p-2 bg-sea-green py-1 px-4 text-white uppercase font-normal rounded-md"
+            >
+              Add User
+            </button>
+          </div>
+          <div className="px-4 pb-2">
+            <input
+              type="text"
+              className="p-3 text-md border outline-none rounded-md w-full md:w-auto md:min-w-[500px]"
+              placeholder="Search User by name or email"
+            />
+          </div>
         </div>
         <UsersTable />
       </div>
