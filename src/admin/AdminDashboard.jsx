7f57aab3776d1e93589c8cec9649a6b6557780cb
Martin Mwangi <mwangimartin1904@gmail.com>
2022-03-04 00:10:59 +0300

    Update leadership admin

--- src/admin/AdminDashboard.jsx
+++ src/admin/AdminDashboard.jsx
@@ -3,35 +3,41 @@ import { Outlet } from "react-router-dom";
 
 import TopBar from "./components/TopBar";
 import SideBar from "./components/SideBar";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getLoggedInAdmin } from "../redux/actions/admin/adminLogin";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const AdminDashboard = () => {
-  const [sideBarOpen, setSideBarOpen] = useState(true);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getLoggedInAdmin());
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  return (
-    <div className="min-w-full min-h-screen  h-screen overflow-hidden bg-blue-100">
-      <TopBar sideBarOpen={sideBarOpen} setSideBarOpen={setSideBarOpen} />
-      <SideBar open={sideBarOpen} />
-      <main
-        className={`${
-          sideBarOpen && "ml-64"
-        } transition-all duration-200 ease-linear pt-16`}
-      >
-        <div className="p-6 h-[90vh] overflow-y-auto">
-          <Outlet />
+    const { admin } = useSelector((state) => state.adminState);
+
+    const [sideBarOpen, setSideBarOpen] = useState(true);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getLoggedInAdmin());
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    if (!admin?._id) {
+        return <div></div>;
+    }
+
+    return (
+        <div className="min-w-full min-h-screen  h-screen overflow-hidden bg-blue-100">
+            <TopBar sideBarOpen={sideBarOpen} setSideBarOpen={setSideBarOpen} />
+            <SideBar open={sideBarOpen} />
+            <main
+                className={`${
+                    sideBarOpen && "ml-64"
+                } transition-all duration-200 ease-linear pt-16`}
+            >
+                <div className="p-6 h-[90vh] overflow-y-auto">
+                    <Outlet />
+                </div>
+            </main>
+            <ToastContainer />
         </div>
-      </main>
-      <ToastContainer />
-    </div>
-  );
+    );
 };
 
 export default AdminDashboard;
--- src/admin/pages/LeadershipPage.jsx
+++ src/admin/pages/LeadershipPage.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React from "react";
+import { Tab } from "@headlessui/react";
 
 const LeadershipPage = () => {
-  return <div>
-      <h4>Leadership Page</h4>
-  </div>;
+    return (
+        <div className="px-2 sm:px-0">
+            <Tab.Group>
+                <Tab.List className="flex flex-wrap">
+                    {[
+                      "Church Leadership",
+                      "St Angelus",
+                      "St Joseph",
+                      "St Peters",
+                      "St Johns",
+                      "St Paul",
+                      "St Catherine",
+                      "St Gabriel",
+                      "St Vitus",
+                    ].map((title, i) => (
+                        <Tab
+                        key={i}
+                            className={({ selected }) =>
+                                `${
+                                    selected
+                                        ? "border-b-2 border-b-slate-900 bg-slate-900 text-white"
+                                        : ""
+                                } py-2 px-4`
+                            }
+                        >
+                            <div className="font-medium">{title}</div>
+                        </Tab>
+                    ))}
+                </Tab.List>
+                <Tab.Panels className="mt-2">
+                    <Tab.Panel>
+                        <h4>Hey</h4>
+                    </Tab.Panel>
+                </Tab.Panels>
+            </Tab.Group>
+        </div>
+    );
 };
 
 export default LeadershipPage;
