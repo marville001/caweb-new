c003db5ed4199548bb259463958e0b932585df62
Martin Mwangi <mwangimartin1904@gmail.com>
2022-03-29 11:56:37 +0300

    Update scc view

--- src/admin/pages/SccPage.jsx
+++ src/admin/pages/SccPage.jsx
@@ -17,10 +17,13 @@ const SccPage = () => {
     useEffect(() => {
         dispatch(getSccAction(key));
     }, [dispatch, key]);
-    
+
     return (
         <div className="px-2 sm:px-0">
-            <Link to="/admin/sccs" className="my-5 flex items-center text-sm space-x-3 cursor-pointer">
+            <Link
+                to="/admin/sccs"
+                className="my-5 flex items-center text-sm space-x-3 cursor-pointer"
+            >
                 <FaChevronLeft /> <span>Go Back</span>
             </Link>
             <h2 className="text-2xl text-dodge-blue font-bold">{scc.name}</h2>
@@ -31,18 +34,23 @@ const SccPage = () => {
                     </div>
                 </div>
             )}
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-6">
-                {sccs.map((scc, idx) => (
-                    <Link
-                        to={`/admin/sccs/${scc.key}`}
-                        key={idx}
-                        className="p-2 py-4 bg-dodge-blue text-center text-white rounded-md
-                        cursor-pointer hover:-translate-y-1 hover:opacity-80
-                        "
-                    >
-                        {scc.title}
-                    </Link>
-                ))}
+            <div className="my-4 w-full sm:w-[400px] overflow-hidden rounded-lg">
+                <img
+                    className="w-full h-52 object-cover object-center"
+                    src={process.env.REACT_APP_UPLOADS_URL + scc.image}
+                    alt=""
+                />
+            </div>
+            <div className="my-4 w-full sm:w-[400px]">
+                <p>{scc.description}</p>
+            </div>
+            <div className="my-4 border-t border-t-red-50 py-2 w-full sm:w-[400px] flex space-x-5">
+                <button className="bg-red-300 text-red-800 py-1 px-5 inline rounded-md cursor-pointer">
+                    Delete
+                </button>
+                <button className="bg-dodge-blue text-white py-1 px-5 inline rounded-md cursor-pointer">
+                    Edit
+                </button>
             </div>
         </div>
     );
