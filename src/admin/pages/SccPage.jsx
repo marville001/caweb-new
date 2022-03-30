d3220bf10d1f94303245dc3aff41e089723f9ff4
Martin Mwangi <mwangimartin1904@gmail.com>
2022-03-30 20:24:12 +0300

    Updates

--- src/admin/pages/SccPage.jsx
+++ src/admin/pages/SccPage.jsx
@@ -32,38 +32,61 @@ const SccPage = () => {
             >
                 <FaChevronLeft /> <span>Go Back</span>
             </Link>
-            {scc.key ? (
-                <>
-                    <h2 className="text-2xl text-dodge-blue font-bold">
-                        {scc.name}
+            <div className="max-w-2xl mx-auto">
+                <div className="bg-white p-6">
+                    <h2 className="font-3xl mb-3 font-bold uppercase opacity-50 tracking-widest font-mono">
+                        About Group
                     </h2>
+                    <div className="w-full h-[2px] bg-gray-500 opacity-25 my-3" />
+                    {scc.key ? (
+                        <>
+                            <div className="flex justify-between items-center">
+                                <h2 className="text-2xl text-dodge-blue">
+                                    {scc.name}
+                                </h2>
+                                <h3 className="opacity-70">Name</h3>
+                            </div>
 
-                    <div className="my-4 w-full sm:w-[400px] overflow-hidden rounded-lg">
-                        <img
-                            className="w-full h-52 object-cover object-center"
-                            src={process.env.REACT_APP_UPLOADS_URL + scc.image}
-                            alt=""
-                        />
-                    </div>
-                    <div className="my-4 w-full sm:w-[400px]">
-                        <p>{scc.description}</p>
-                    </div>
-                    <div className="my-4 border-t border-t-red-50 py-2 w-full sm:w-[400px] flex space-x-5">
-                        <button className="bg-red-300 text-red-800 py-1 px-5 inline rounded-md cursor-pointer">
-                            Delete
-                        </button>
-                        <button className="bg-dodge-blue text-white py-1 px-5 inline rounded-md cursor-pointer">
-                            Edit
-                        </button>
-                    </div>
-                </>
-            ) : (
-                <div>
-                    <h4 className="font-mono text-dodge-blue font-bold">
-                        Scc not Added yet
-                    </h4>
+                            <div className="my-4 w-full overflow-hidden rounded-lg flex gap-3">
+                                <img
+                                    className="w-full sm:w-1/2 rounded-lg h-52 object-cover object-center cursor-pointer"
+                                    src={
+                                        process.env.REACT_APP_UPLOADS_URL +
+                                        scc.image
+                                    }
+                                    alt=""
+                                />
+                            </div>
+                            <button className="bg-dodge-blue px-5 py-1 text-white text-sm">Add Image</button>
+                            <div className="my-4 w-full">
+                                <p>{scc.description}</p>
+                            </div>
+                            <div className="my-4 border-t border-t-red-50 py-2 w-full sm:w-[400px] flex space-x-5">
+                                <button className="bg-red-300 text-red-800 py-1 px-5 inline rounded-md cursor-pointer">
+                                    Delete
+                                </button>
+                                <button className="bg-dodge-blue text-white py-1 px-5 inline rounded-md cursor-pointer">
+                                    Edit
+                                </button>
+                            </div>
+                        </>
+                    ) : (
+                        <div>
+                            <h4 className="font-mono text-dodge-blue font-bold">
+                                Scc not Added yet
+                            </h4>
+                        </div>
+                    )}
                 </div>
-            )}
+
+                <div className="bg-white p-6 mt-3">
+                    <h2 className="font-3xl mb-3 font-bold uppercase opacity-50 tracking-widest font-mono">
+                        Scc Events
+                    </h2>
+                    <div className="w-full h-[2px] bg-gray-500 opacity-25 my-3" />
+                    <input type="text" />
+                </div>
+            </div>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,25 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  darkMode: 'class',
-  theme: {
-    container: {
-      center: true,
-      padding: "1rem",
-      screens: {
-        lg: "1124px",
-        xl: "1124px",
-        "2xl": "1124px",
-      },
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    darkMode: "class",
+    theme: {
+        container: {
+            center: true,
+            padding: "1rem",
+            screens: {
+                lg: "1124px",
+                xl: "1124px",
+                "2xl": "1124px",
+            },
+        },
+        extend: {
+            colors: {
+                "dodge-blue": "#236092",
+                "sea-green": "#407b54",
+            },
+        },
     },
-    extend: {
-      colors: {
-        "dodge-blue": "#236092",
-        "sea-green": "#407b54",
-      },
-    },
-  },
-  plugins: [require('@tailwindcss/typography')],
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+    ],
 };
