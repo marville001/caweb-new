17c0f23ffc5ff45d31c72a31024bb549cfd29b52
Martin Mwangi <mwangimartin1904@gmail.com>
2022-05-18 21:25:14 +0300

    Updates

--- src/admin/AdminDashboard.jsx
+++ src/admin/AdminDashboard.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 
-import TopBar from "./components/TopBar";
 import SideBar from "./components/SideBar";
 import { useDispatch } from "react-redux";
 import { getLoggedInAdmin } from "../redux/actions/admin/adminLogin";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Header from "./components/Header";
 
 const AdminDashboard = () => {
 
@@ -39,12 +39,12 @@ const AdminDashboard = () => {
 
     return (
         <div className="min-w-full min-h-screen  h-screen overflow-hidden bg-blue-100">
-            <TopBar sideBarOpen={sideBarOpen} setSideBarOpen={setSideBarOpen} />
+            <Header sidebarOpen={sideBarOpen} setSidebarOpen={setSideBarOpen} />
             <SideBar open={sideBarOpen} />
             <main
                 className={`${
                     sideBarOpen && "xl:ml-64"
-                } transition-all duration-200 ease-linear pt-16`}
+                } transition-all duration-200 ease-linear`}
             >
                 <div className="p-6 h-[90vh] overflow-y-auto">
                     <Outlet />
