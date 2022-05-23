bc35f5d5f26a4f2dcd33d1dd3dad464da9de0244
Martin Mwangi <mwangimartin1904@gmail.com>
2022-05-24 00:01:09 +0300

    Update leadership page

--- src/pages/leadership/SccLeadershipWelcome.jsx
+++ src/pages/leadership/SccLeadershipWelcome.jsx
@@ -1,15 +1,18 @@
+import { useEffect } from "react";
+import { FaSpinner } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-
-const sccs = [
-    { title: "St Angelus", key: "stangelus", subtitle: "the dancers" },
-    { title: "St Peters", key: "stpeters", subtitle: "the dancers" },
-    { title: "St Joseph", key: "stangelus", subtitle: "the dancers" },
-    { title: "St Gabriel", key: "stgabriel", subtitle: "the dancers" },
-    { title: "St Paul Gents", key: "stpaul", subtitle: "the dancers" },
-    { title: "St Catherine Ladies", key: "stcatherine", subtitle: "the dancers" },
-];
+import { getSccsAction } from "../../redux/actions/admin/sccs";
 
 export default function SccLeadershipWelcome() {
+    const { sccs, isLoadingSccs } = useSelector((state) => state.sccsState);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getSccsAction());
+    }, [dispatch]);
+
     return (
         <div className="container">
             <div className="w-full px-2 py-16 sm:px-0">
@@ -20,22 +23,29 @@ export default function SccLeadershipWelcome() {
                     Please select Scc from list below
                 </p>
 
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 my-6 gap-5">
-                    {sccs.map(({ key, title, subtitle }) => (
-                        <Link
-                            to={`/leadership/scc/${key}`}
-                            key={key}
-                            className="
+                {isLoadingSccs ? (
+                    <div className="flex py-10 justify-center">
+                        <FaSpinner className="animate-spin text-lg" />
+                    </div>
+                ) : (
+
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 my-6 gap-5">
+                        {sccs.map(({ key, name }) => (
+                            <Link
+                                to={`/leadership/scc/${key}`}
+                                key={key}
+                                className="
                           bg-white py-4 shadow-md text-center  hover:-translate-y-1
                             hover:bg-dodge-blue hover:text-white rounded-md sm:py-6 
                             transition-all duration-150 ease-in group
                         "
-                        >
-                            <h2 className="text-md font-bold text-dodge-blue group-hover:text-white">{title}</h2>
-                            <p className="text-sm italic mt-2">{subtitle}</p>
-                        </Link>
-                    ))}
-                </div>
+                            >
+                                <h2 className="text-md font-bold text-dodge-blue group-hover:text-white">{name}</h2>
+                                {/* <p className="text-sm italic mt-2">{name}</p> */}
+                            </Link>
+                        ))}
+                    </div>
+                )}
             </div>
         </div>
     );
