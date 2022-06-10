ec5cd91344ac78106c2b0e1a7c03547b3e75c292
Martin Mwangi <mwangimartin1904@gmail.com>
2022-06-10 12:52:50 +0300

    Updates

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -48,11 +48,13 @@ const News = () => {
                 ))}
             </div>
 
-            <div className="py-6 flex justify-center">
-                <button className="bg-sea-green text-white py-3  px-8 text-lg font-medium -tracking-tighter hover:opacity-90">
-                    Load More
-                </button>
-            </div>
+            {9 > 10 && (
+                <div className="py-6 flex justify-center">
+                    <button className="bg-sea-green text-white py-3  px-8 text-lg font-medium -tracking-tighter hover:opacity-90">
+                        Load More
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
