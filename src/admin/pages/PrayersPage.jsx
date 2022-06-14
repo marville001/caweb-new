b90d34e90f3eac55273fc362b6c678631a327f09
Martin Mwangi <mwangimartin1904@gmail.com>
2022-06-14 23:08:17 +0300

    :sparkles: UI for deleting prayer

--- src/admin/pages/PrayersPage.jsx
+++ src/admin/pages/PrayersPage.jsx
@@ -2,91 +2,117 @@ import React, { useEffect, useState } from "react";
 import { FaSpinner } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { getPrayers } from "../../redux/actions/admin/prayers";
+import ConfirmDeleteModal from "../components/common/ConfirmDeleteModal";
 import AddPrayerModal from "../components/PrayersPageComponents/AddPrayerModal";
 import EditPrayerModal from "../components/PrayersPageComponents/EditPrayerModal";
 const PrayersPage = () => {
-  const { prayers, isLoadingPrayers, error } = useSelector(
-    (state) => state.prayersState
-  );
-  let [addPrayerModalOpen, setAddPrayerModalOpen] = useState(false);
-  let [editPrayerModalOpen, setEditPrayerModalOpen] = useState(false);
-  const [editPrayer, setEditPrayer] = useState({});
+    const { prayers, isLoadingPrayers, error } = useSelector(
+        (state) => state.prayersState
+    );
+    const [addPrayerModalOpen, setAddPrayerModalOpen] = useState(false);
+    const [editPrayerModalOpen, setEditPrayerModalOpen] = useState(false);
+    const [deletePrayerModalOpen, setDeletePrayerModalOpen] = useState(false);
+    const [editPrayer, setEditPrayer] = useState({});
+    const [deletePrayer, setDeletePrayer] = useState({});
 
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const closeAddPrayerModal = () => {
-    setAddPrayerModalOpen(false);
-  };
+    const closeAddPrayerModal = () => {
+        setAddPrayerModalOpen(false);
+    };
 
-  const openAddPrayerModal = () => {
-    setAddPrayerModalOpen(true);
-  };
+    const openAddPrayerModal = () => {
+        setAddPrayerModalOpen(true);
+    };
 
-  useEffect(() => {
-    dispatch(getPrayers());
-  }, [dispatch]);
+    const handleDeletePrayer = async () => {};
 
-  return (
-    <div className="rounded-md  p-4">
-      <div className="flex justify-between">
-        <h4 className="text-2xl text-dodge-blue font-bold">Prayers</h4>
-        <button
-          onClick={openAddPrayerModal}
-          className="p-2 bg-sea-green py-1 px-4 text-white uppercase font-normal rounded-md"
-        >
-          Add Prayer
-        </button>
-      </div>
-      {error && (
-        <div className="bg-red-100 p-2 flex justify-center">
-          <p className="text-red-500">{error}</p>
-        </div>
-      )}
-      {isLoadingPrayers && (
-        <div className="my-5 flex items-center justify-center">
-          <div className="animate-spin">
-            <FaSpinner className="w-8 h-8" />
-          </div>
-        </div>
-      )}
+    useEffect(() => {
+        dispatch(getPrayers());
+    }, [dispatch]);
 
-      <div className="my-8 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {prayers.map(({ _id, prayer, title }) => (
-          <div key={_id} className="bg-white _shadow py-2 px-6 border-2 rounded-md">
-            <h2 className="text-md my-2 font-medium">{title}</h2>
-            <p className="text-sm text-justify">
-              {prayer.substring(0, 120)} {prayer.length > 120 && " ..."}
-            </p>
-            <div className="flex justify-between mt-4">
-              <button
-                onClick={() => {
-                  setEditPrayer({ _id, prayer, title });
-                  setEditPrayerModalOpen(true);
-                }}
-                className="bg-dodge-blue px-2 rounded text-white text-xs py-1"
-              >
-                Edit / View
-              </button>
-              <button disabled className="cursor-not-allowed bg-red-300 px-2 rounded text-white text-xs py-1">
-                Delete
-              </button>
+    return (
+        <div className="rounded-md  p-4">
+            <div className="flex justify-between">
+                <h4 className="text-2xl text-dodge-blue font-bold">Prayers</h4>
+                <button
+                    onClick={openAddPrayerModal}
+                    className="p-2 bg-sea-green py-1 px-4 text-white uppercase font-normal rounded-md"
+                >
+                    Add Prayer
+                </button>
             </div>
-          </div>
-        ))}
-      </div>
+            {error && (
+                <div className="bg-red-100 p-2 flex justify-center">
+                    <p className="text-red-500">{error}</p>
+                </div>
+            )}
+            {isLoadingPrayers && (
+                <div className="my-5 flex items-center justify-center">
+                    <div className="animate-spin">
+                        <FaSpinner className="w-8 h-8" />
+                    </div>
+                </div>
+            )}
 
-      <AddPrayerModal
-        isOpen={addPrayerModalOpen}
-        closeModal={closeAddPrayerModal}
-      />
-      <EditPrayerModal
-        isOpen={editPrayerModalOpen}
-        editPrayer={editPrayer}
-        setEditPrayer={setEditPrayer}
-        closeModal={() => setEditPrayerModalOpen(false)}
-      />
-    </div>
-  );
+            <div className="my-8 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                {prayers.map(({ _id, prayer, title }) => (
+                    <div
+                        key={_id}
+                        className="bg-white _shadow py-2 px-6 border-2 rounded-md"
+                    >
+                        <h2 className="text-md my-2 font-medium">{title}</h2>
+                        <p className="text-sm text-justify">
+                            {prayer.substring(0, 120)}{" "}
+                            {prayer.length > 120 && " ..."}
+                        </p>
+                        <div className="flex justify-between mt-4">
+                            <button
+                                onClick={() => {
+                                    setEditPrayer({ _id, prayer, title });
+                                    setEditPrayerModalOpen(true);
+                                }}
+                                className="bg-dodge-blue px-2 rounded text-white text-xs py-1"
+                            >
+                                Edit / View
+                            </button>
+                            <button
+                                onClick={() => {
+                                    setDeletePrayer({ _id, prayer, title });
+                                    setDeletePrayerModalOpen(true);
+                                }}
+                                className="cursor-pointer bg-red-500 px-2 rounded text-white text-xs py-1"
+                            >
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            <AddPrayerModal
+                isOpen={addPrayerModalOpen}
+                closeModal={closeAddPrayerModal}
+            />
+            <EditPrayerModal
+                isOpen={editPrayerModalOpen}
+                editPrayer={editPrayer}
+                setEditPrayer={setEditPrayer}
+                closeModal={() => setEditPrayerModalOpen(false)}
+            />
+
+            <ConfirmDeleteModal
+                isOpen={deletePrayerModalOpen}
+                closeModal={() => {
+                    setDeletePrayer({});
+                    setDeletePrayerModalOpen(false);
+                }}
+                loading={true}
+                message={`Please Confirm Deleting the Prayer {${deletePrayer?.title}}. This will erase all data about the prayer`}
+                actionMethod={handleDeletePrayer}
+            />
+        </div>
+    );
 };
 
 export default PrayersPage;
