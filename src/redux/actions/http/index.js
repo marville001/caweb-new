f8d49199ca1c3437986811c764148d72738cda16
Martin Mwangi <mwangimartin1904@gmail.com>
2022-06-16 18:07:31 +0300

    Updates

--- src/redux/actions/http/index.js
+++ src/redux/actions/http/index.js
@@ -4,3 +4,4 @@ export { put } from "./put";
 export { _delete } from "./_delete";
 
 export const API_URL = "https://caweb-api.herokuapp.com/api/"
+// export const API_URL = "http://api.dekutcatholicchaplaincy.org/api/"
