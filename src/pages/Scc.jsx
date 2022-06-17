6ede02d2abcc86ff8dc6ee597112ab316bafcfee
Martin Mwangi <mwangimartin1904@gmail.com>
2022-06-17 08:58:20 +0300

    Updates

--- src/pages/Scc.jsx
+++ src/pages/Scc.jsx
@@ -112,6 +112,19 @@ const Scc = () => {
                                 ))}
                         </div>
                     </div>
+
+                    <div className="py-10">
+                        <h2 className="uppercase mb-8 text-center text-lg text-slate-800 font-medium">
+                            Bible study GROUPS
+                        </h2>
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                            {sccs
+                                ?.filter((scc) => scc.category === "bible-study")
+                                ?.map((scc) => (
+                                    <SccCard key={scc._id} scc={scc} />
+                                ))}
+                        </div>
+                    </div>
                 </>
             )}
         </div>
