2d98f491b9e9ddc1975d3c25c80fdcb96b772432
Martin Mwangi <mwangimartin1904@gmail.com>
2022-06-18 09:34:37 +0300

    Updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -82,13 +82,17 @@ const Home = () => {
                             </h1>
                             <div className="text-slate-100 md:w-4/6 text-lg font-light my-4 grid-cols-2 grid sm:grid-cols-3 gap-x-5 sm:min-w-[600px] pr-16">
                                 <span className="mr-5 block">Wisdom</span>
-                                <span className="mr-5 block">Understanding</span>
+                                <span className="mr-5 block">
+                                    Understanding
+                                </span>
                                 <span className="mr-5 block">Counsel</span>
                                 <span className="mr-5 block">Knowledge</span>
                                 <span className="mr-5 block">Fortitude</span>
                                 <span className="mr-5 block">Piety</span>
                                 <span className="mr-5 block">Piety</span>
-                                <span className="mr-5 block">Fear of the Lord</span>
+                                <span className="mr-5 block">
+                                    Fear of the Lord
+                                </span>
                             </div>
                             <Link
                                 to="/about"
@@ -101,17 +105,21 @@ const Home = () => {
                     <SwiperSlide>
                         <div className="md:container min-h-[400px] p-4 px-10 w-full bg-[#0b416c] md:bg-inherit md:h-[70vh] grid md:place-content-center ">
                             <h1 className="text-white font-semibold text-3xl md:text-5xl mb-2">
-                                Gifts of the Holy Spirit
+                                Fruits of the Holy Spirit
                             </h1>
                             <div className="text-slate-100 md:w-4/6 text-lg font-light my-4 grid-cols-2 grid sm:grid-cols-3 gap-x-5 sm:min-w-[600px] pr-16">
-                                <span className="mr-5 block">Wisdom</span>
-                                <span className="mr-5 block">Understanding</span>
-                                <span className="mr-5 block">Counsel</span>
-                                <span className="mr-5 block">Knowledge</span>
-                                <span className="mr-5 block">Fortitude</span>
-                                <span className="mr-5 block">Piety</span>
-                                <span className="mr-5 block">Piety</span>
-                                <span className="mr-5 block">Fear of the Lord</span>
+                                <span className="mr-5 block">Charity</span>
+                                <span className="mr-5 block">Joy</span>
+                                <span className="mr-5 block">Peace</span>
+                                <span className="mr-5 block">Patience</span>
+                                <span className="mr-5 block">Kindness</span>
+                                <span className="mr-5 block">Goodness</span>
+                                <span className="mr-5 block">Generosity</span>
+                                <span className="mr-5 block">Gentleness</span>
+                                <span className="mr-5 block">Faithfulness</span>
+                                <span className="mr-5 block">Modesty</span>
+                                <span className="mr-5 block">Self Control</span>
+                                <span className="mr-5 block">Chastity</span>
                             </div>
                             <Link
                                 to="/about"
