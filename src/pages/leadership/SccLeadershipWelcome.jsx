095db735c345edc00b69928fb08f8194a6b82b94
Martin Mwangi <mwangimartin1904@gmail.com>
2022-06-18 09:40:41 +0300

    Updates

--- src/pages/leadership/SccLeadershipWelcome.jsx
+++ src/pages/leadership/SccLeadershipWelcome.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { FaSpinner } from "react-icons/fa";
+import { FaBible, FaSpinner } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { getSccsAction } from "../../redux/actions/admin/sccs";
@@ -29,19 +29,20 @@ export default function SccLeadershipWelcome() {
                     </div>
                 ) : (
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 my-6 gap-5">
-                        {sccs.map(({ key, name, _id }) => (
+                        {sccs.map(({ key, name,category, _id }) => (
                             <Link
                                 to={`/scc/leaders/${_id}`}
                                 key={key}
                                 className="
                           bg-white py-4 px-2 shadow-md text-center  hover:-translate-y-1
                             hover:bg-dodge-blue hover:text-white rounded-md sm:py-6 
-                            transition-all duration-150 ease-in group
+                            transition-all duration-150 ease-in group  flex justify-center items-center gap-4
                         "
                             >
                                 <h2 className="text-md font-bold text-dodge-blue group-hover:text-white">
                                     {name}
                                 </h2>
+                                {category === "bible-study" && <FaBible className="text-dodge-blue group-hover:text-white" />}
                                 {/* <p className="text-sm italic mt-2">{name}</p> */}
                             </Link>
                         ))}
