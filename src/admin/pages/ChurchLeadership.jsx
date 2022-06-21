ba6e397fba441595b382385dd8674f15247156d1
Martin Mwangi <mwangimartin1904@gmail.com>
2022-06-21 21:31:30 +0300

    Update leadership page

--- src/admin/pages/ChurchLeadership.jsx
+++ src/admin/pages/ChurchLeadership.jsx
@@ -23,62 +23,54 @@ const ChurchLeadership = () => {
     }, [dispatch]);
     return (
         <div className="px-2 sm:px-0">
-            <div className="">
-                <div className="my-6 p-4 bg-white _shadow">
-                    <div
-                        onClick={() => setShowPositions((prev) => !prev)}
-                        className="flex gap-5 justify-between cursor-pointer items-center"
-                    >
-                        <h2 className="font-bold text-lg">
-                            Leadership Positions
-                        </h2>
+            <div className="my-6 p-2 sm:p-4 bg-white _shadow">
+                <div
+                    onClick={() => setShowPositions((prev) => !prev)}
+                    className="flex gap-5 justify-between cursor-pointer items-center"
+                >
+                    <h2 className="font-bold text-lg">Leadership Positions</h2>
 
-                        <div className="flex items-center space-x-2 py-2 cursor-pointer px-6 rounded-md text-seagreen  text-sm hover:opacity-75">
-                            {showPositions ? (
-                                <FaChevronUp />
-                            ) : (
-                                <FaChevronDown />
-                            )}
-                        </div>
+                    <div className="flex items-center space-x-2 py-2 cursor-pointer px-6 rounded-md text-seagreen  text-sm hover:opacity-75">
+                        {showPositions ? <FaChevronUp /> : <FaChevronDown />}
                     </div>
-                    <div
-                        className={`grid grid-cols-1 transition-all duration-150 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 my-5 
+                </div>
+                <div
+                    className={`grid grid-cols-1 transition-all duration-150 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 my-5 
                         ${showPositions ? "" : "h-0 hidden  overflow-hidden"}`}
-                    >
-                        {positions.map((position) => (
-                            <Link
-                                to={`/admin/leaders/position/${position._id}`}
-                                key={position._id}
-                                className="_shadow px-6 py-3 flex items-center justify-center text-seagreen rounded-md hover:scale-[1.02] 
+                >
+                    {positions.map((position) => (
+                        <Link
+                            to={`/admin/leaders/position/${position._id}`}
+                            key={position._id}
+                            className="_shadow px-6 py-3 flex items-center justify-center text-seagreen rounded-md hover:scale-[1.02] 
                                 transition-all duration-150 ease-linear cursor-pointer
                                 hover:opacity-80 hover:bg-slate-50"
-                            >
-                                <span>{position?.title}</span>
-                            </Link>
-                        ))}
-                        <Link
-                            to="/admin/leaders/position/new"
-                            className="_shadow bg-steelblue bg-opacity-20 text-steelblue rounded-md px-6 py-3 flex items-center justify-center 
-                                hover:scale-[1.02] transition-all duration-150 ease-linear cursor-pointer hover:bg-opacity-40"
                         >
-                            <span>Add Position</span>
+                            <span>{position?.title}</span>
                         </Link>
-                    </div>
-
-                    {showPositions && (
-                        <div className="my-2">
-                            <button
-                                onClick={() => setShowPositions(false)}
-                                className="rounded bg-dodge-blue text-white text-sm px-6 py-1"
-                            >
-                                Hide
-                            </button>
-                        </div>
-                    )}
+                    ))}
+                    <Link
+                        to="/admin/leaders/position/new"
+                        className="_shadow bg-steelblue bg-opacity-20 text-steelblue rounded-md px-6 py-3 flex items-center justify-center 
+                                hover:scale-[1.02] transition-all duration-150 ease-linear cursor-pointer hover:bg-opacity-40"
+                    >
+                        <span>Add Position</span>
+                    </Link>
                 </div>
+
+                {showPositions && (
+                    <div className="my-2">
+                        <button
+                            onClick={() => setShowPositions(false)}
+                            className="rounded bg-dodge-blue text-white text-sm px-6 py-1"
+                        >
+                            Hide
+                        </button>
+                    </div>
+                )}
             </div>
 
-            <div className="my-6 p-4 bg-white _shadow">
+            <div className="my-6 p-2 sm:p-4 bg-white _shadow">
                 <div
                     onClick={() => setShowLeadership((prev) => !prev)}
                     className="flex gap-5 justify-between cursor-pointer items-center"
@@ -134,7 +126,7 @@ const ChurchLeadership = () => {
                 </div>
             </div>
 
-            <div className="my-6 p-4 bg-white _shadow">
+            <div className="my-6 p-2 sm:p-4 bg-white _shadow">
                 <div
                     onClick={() => setShowCommittee((prev) => !prev)}
                     className="flex gap-5 justify-between cursor-pointer items-center"
