4c1f88d6268f44d0e66955a02ade777a1c79173c
Martin Mwangi <mwangimartin1904@gmail.com>
2022-06-21 09:00:09 +0300

    Updates

--- src/redux/actions/http/index.js
+++ src/redux/actions/http/index.js
@@ -8,4 +8,4 @@ export const API_URL =
     process.env.NODE_ENV === "development"
         ? "http://localhost:5500/api/"
         : "https://caweb-api.herokuapp.com/api/";
-// export const API_URL = "http://api.dekutcatholicchaplaincy.org/api/"
+// export const API_URL = "https://api.dekutcatholicchaplaincy.org/api/"
